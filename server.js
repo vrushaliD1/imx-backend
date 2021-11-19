6982dbcccc0dd4f2d4400e5c60609503928b6af7
safari-creator <evansmusafa@gmail.com>
2021-11-20 02:01:43 +0530

    'change'

--- server.js
+++ server.js
@@ -32,6 +32,8 @@ app.post("/check-address", function (req, res) {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Listening on port 5000");
-});
+app.listen(process.env.PORT || 5000)
+
+// app.listen(5000, () => {
+//   console.log("Listening on port 5000");
+// });
